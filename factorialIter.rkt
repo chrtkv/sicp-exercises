c11de18ebbeca154a0d86465bb2d2d04fa1c1456
Kirill <chertkov.k@gmail.com>
2018-05-03 19:47:38 +0300

    rename variable

--- factorialIter.rkt
+++ factorialIter.rkt
@@ -1,5 +1,5 @@
 #lang racket
 (define (factorial x)
-  (define (factorial-iter factorial counter x)
-    (if (> counter x) factorial (factorial-iter (* factorial counter) (+ counter 1) x)))
+  (define (factorial-iter fact counter x)
+    (if (> counter x) fact (factorial-iter (* fact counter) (+ counter 1) x)))
   (factorial-iter 1 1 x))
